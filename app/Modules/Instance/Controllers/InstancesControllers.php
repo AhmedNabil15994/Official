493d397c1036c8ad58856610bc481568e0e4e75d
AhmedNabil94 <mody998866@gmail.com>
2023-02-09 02:13:13 +0200

    Fix

--- app/Modules/Instance/Controllers/InstancesControllers.php
+++ app/Modules/Instance/Controllers/InstancesControllers.php
@@ -52,6 +52,7 @@ class InstancesControllers extends Controller {
             $data['status'] = \TraitsFunc::SuccessResponse();
             return \Response::json((object) $data);        
         }
+        
         $deviceObj = Device::NotDeleted()->where('name',$name)->first();
         if($deviceObj->multidevice == "YES"){
             $islegacy = "false"; 
@@ -60,7 +61,6 @@ class InstancesControllers extends Controller {
         }
         $response = Http::post(env('URL_WA_SERVER').'/sessions/add', ['id' => $name, 'isLegacy' => $islegacy]);
         $res = json_decode($response->getBody());
-        dd($res );
         if(!$res->success){
             $image = $deviceObj->image;
         }else{
